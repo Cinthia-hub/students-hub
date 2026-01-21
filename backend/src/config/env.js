ba3f103824d090c2c86ad12b3cf432468d12cb87
Cinthia-hub <ccamibrm@gmail.com>
2026-01-21 13:13:53 -0600

    [EDITED]: Edite algunas cuestiones

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -6,7 +6,6 @@ export const env ={
     FIREBASE_PROJECT_ID:process.env.FIREBASE_PROJECT_ID,
     FIREBASE_CLIENT_EMAIL:process.env.FIREBASE_CLIENT_EMAIL,
     FIREBASE_PRIVATE_KEY:process.env.FIREBASE_PRIVATE_KEY,
-
     JWT_SECRET:process.env.JWT_SECRET,
     JWT_EXPIRES_IN:process.env.JWT_EXPIRES_IN
 }
